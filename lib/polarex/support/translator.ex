0bc39bfd629e6cb97cce634cc5fc27177f9c03dc
Gianpiero Spinelli <gianpispi@me.com>
2026-05-11 09:34:33 +0300

    fix: translation issues (#9)
    * fix date-time translation
    
    ``` elixir
    ** (RuntimeError) Response translation not implemented: {:string, "date-time"}
        (polarex 0.3.1) lib/polarex/support/translator.ex:71: Polarex.Support.Translator.translate/2
        (polarex 0.3.1) lib/polarex/support/translator.ex:47: anonymous fn/2 in Polarex.Support.Translator.translate/2
        (elixir 1.19.5) lib/enum.ex:1688: Enum."-map/2-lists^map/1-1-"/2
        (elixir 1.19.5) lib/map.ex:267: Map.new_from_enum/2
        (polarex 0.3.1) lib/polarex/support/translator.ex:44: Polarex.Support.Translator.translate/2
        (polarex 0.3.1) lib/polarex/support/client.ex:16: Polarex.Support.Client.request/1
        (specto_core 0.1.0) lib/s/subscription/provider/implementation/polar.ex:19: Sub.Provider.Polar.get_user_portal_url/1
        iex:1: (file)
    ```
    
    * fix map translation
    
    ``` elixir
    ** (FunctionClauseError) no function clause matching in anonymous fn/1 in Polarex.Support.Translator.translate/2
    
        The following arguments were given to anonymous fn/1 in Polarex.Support.Translator.translate/2:
    
            # 1
            :map
    
        (polarex 0.3.1) lib/polarex/support/translator.ex:30: anonymous fn/1 in Polarex.Support.Translator.translate/2
        (elixir 1.19.5) lib/enum.ex:4459: Enum.find_list/3
        (polarex 0.3.1) lib/polarex/support/translator.ex:30: Polarex.Support.Translator.translate/2
        (polarex 0.3.1) lib/polarex/support/translator.ex:52: anonymous fn/2 in Polarex.Support.Translator.translate/2
        (elixir 1.19.5) lib/enum.ex:1688: Enum."-map/2-lists^map/1-1-"/2
        (elixir 1.19.5) lib/enum.ex:1688: Enum."-map/2-lists^map/1-1-"/2
        (elixir 1.19.5) lib/map.ex:267: Map.new_from_enum/2
        iex:2: (file)
    ```
    
    * add tests for recent bug fix
    
    * fix possible other failures in translating the payload

--- lib/polarex/support/translator.ex
+++ lib/polarex/support/translator.ex
@@ -8,27 +8,30 @@ defmodule Polarex.Support.Translator do
   def translate(nil, body), do: body
   def translate(:map, body), do: body
   def translate([:map], body), do: body
+  def translate(:string, body), do: body
   def translate({:string, :generic}, body), do: body
+  def translate({:string, :date_time}, body), do: parse_datetime!(body)
+  def translate({:string, "date-time"}, body), do: parse_datetime!(body)
+  def translate({:string, "date"}, body), do: Date.from_iso8601!(body)
+  def translate({:string, _format}, body), do: body
   def translate(:boolean, body), do: body
   def translate(:integer, body) when is_binary(body), do: String.to_integer(body)
   def translate(:integer, body), do: body
-  def translate({:string, :date_time}, body), do: NaiveDateTime.from_iso8601!(body)
+  def translate(:number, body), do: body
+  def translate(:unknown, body), do: body
   def translate({:const, value}, _body), do: value
+  def translate([const: value], body), do: translate({:const, value}, body)
+  def translate([string: format], body), do: translate({:string, format}, body)
+  def translate([union: types], body), do: translate({:union, types}, body)
 
   def translate({:enum, _values}, body), do: body
 
   def translate({:union, [type, :null]}, body), do: translate(type, body)
 
   def translate({:union, types}, body) do
-    actual_module =
-      types
-      |> Enum.find(fn {module, :t} ->
-        {:const, custom_name} = module.__fields__(:t)[:type]
-        %{"type" => actual_type} = body
-        custom_name == actual_type
-      end)
+    type = Enum.find(types, &matches_type?(&1, body)) || List.first(types)
 
-    translate(actual_module, body)
+    translate(type, body)
   end
 
   def translate({_module, :t}, body) when map_size(body) == 0, do: %{}
@@ -61,14 +64,49 @@ defmodule Polarex.Support.Translator do
     struct!(module, translated)
   end
 
-  def translate([{module, type}], body) when is_list(body) do
-    Enum.map(body, &translate({module, type}, &1))
+  def translate([type], body) when is_list(body) do
+    Enum.map(body, &translate(type, &1))
   end
 
-  def translate([{_module, _type}], nil), do: nil
+  def translate([_type], nil), do: nil
 
   def translate(type, _body) do
     raise("Response translation not implemented: #{inspect(type)}")
+  end
+
+  defp matches_type?(:null, body), do: is_nil(body)
+  defp matches_type?(:map, body), do: is_map(body)
+  defp matches_type?(:string, body), do: is_binary(body)
+  defp matches_type?({:string, _format}, body), do: is_binary(body)
+  defp matches_type?(:boolean, body), do: is_boolean(body)
+  defp matches_type?(:integer, body), do: is_integer(body)
+  defp matches_type?(:number, body), do: is_number(body)
+  defp matches_type?(:unknown, _body), do: true
+  defp matches_type?({:enum, values}, body), do: body in values
+  defp matches_type?({:const, value}, body), do: body == value
+  defp matches_type?([const: value], body), do: matches_type?({:const, value}, body)
+  defp matches_type?([string: format], body), do: matches_type?({:string, format}, body)
+  defp matches_type?([union: types], body), do: matches_type?({:union, types}, body)
+
+  defp matches_type?([type], body),
+    do: is_list(body) and Enum.all?(body, &matches_type?(type, &1))
+
+  defp matches_type?({:union, types}, body), do: Enum.any?(types, &matches_type?(&1, body))
+
+  defp matches_type?({module, :t}, body) when is_atom(module) and is_map(body) do
+    case module.__fields__(:t)[:type] do
+      {:const, type} -> body["type"] == type
+      _other -> true
+    end
+  end
+
+  defp matches_type?(_type, _body), do: false
+
+  defp parse_datetime!(body) do
+    case DateTime.from_iso8601(body) do
+      {:ok, datetime, _offset} -> datetime
+      {:error, _reason} -> NaiveDateTime.from_iso8601!(body)
+    end
   end
 
   # Get the camel-case version of the field from the API payload
